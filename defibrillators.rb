2ddc59951c9dd59d519ec44f03e37dd406c7692f
Slavunderkind <smcankova@gmail.com>
2017-01-11 15:55:29 +0200

    Update solution for defibrilators task

--- defibrillators.rb
+++ defibrillators.rb
@@ -1,60 +1,58 @@
 # Solution for https://www.codingame.com/ide/puzzle/defibrillators
-
 class Defibrillators
-  attr_accessor :lon, :lat, :n
+  attr_accessor :lon, :lat, :n, :defibrilators
 
   def initialize
     @lon = parse(gets.chomp)
     @lat = parse(gets.chomp)
     @n = gets.to_i
+    @defibrilators = {}
   end
 
   def calculate_x(d_long, d_lat)
-      c = Math.cos((lat + d_lat) / 2)
-      (d_long - lon) * c
+    c = Math.cos((lat + d_lat) / 2)
+    (d_long - lon) * c
   end
 
   def calculate_y(d_lat)
-      d_lat - lat
+    d_lat - lat
   end
 
-  def destination(x, y)
-     Math.sqrt(x*x + y*y) * 6371
+  def distance(x, y)
+    Math.sqrt(x * x + y * y) * 6371
   end
 
   def parse(var)
-      (var.sub(',', '.').to_f * Math::PI) / 180
+    (var.sub(',', '.').to_f * Math::PI) / 180
   end
 
   def find_closest(defibrilators_hash)
-      min = defibrilators_hash.values.first
-      closest_name = defibrilators_hash.keys.first
+    min = defibrilators_hash.values.first
+    closest_name = defibrilators_hash.keys.first
 
-      defibrilators_hash.map do |key, value|
-         if value < min
-            min = value
-            closest_name = key
-         end
+    defibrilators_hash.map do |key, value|
+      if value < min
+        min = value
+        closest_name = key
       end
-      closest_name
+    end
+    closest_name
+  end
+
+  def init_defibrilators(defibrilator)
+    d_long = parse(defibrilator[4])
+    d_lat = parse(defibrilator[5])
+    x = calculate_x(d_long, d_lat)
+    y = calculate_y(d_lat)
+    defibrilators[defibrilator[1]] = distance(x, y)
   end
 
   def start
-    defibri = Hash.new
-
     n.times do
-        defib = gets.chomp
-        defib_array = defib.split(';')
-
-        d_long = parse defib_array[4]
-        d_lat = parse defib_array[5]
-        x = calculate_x(d_long, d_lat)
-        y = calculate_y(d_lat)
-        distance = destination(x, y)
-
-        defibri[defib_array[1]] = distance
+      defib = gets.chomp
+      init_defibrilators(defib.split(';'))
     end
-    puts find_closest(defibri)
+    puts find_closest(defibrillators)
   end
 end
 
