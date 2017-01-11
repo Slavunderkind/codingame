eed27ce3c220acc69e61a6625fbef6adbc81b30b
Slavunderkind <smcankova@gmail.com>
2017-01-11 15:25:41 +0200

    Update solution for horse racing task

--- horse_racing.rb
+++ horse_racing.rb
@@ -1,19 +1,24 @@
 # Solution for https://www.codingame.com/ide/puzzle/horse-racing-duals
 class HorseRacing
-  def self.find_difference
-    strenghts = start.sort
+  def find_difference
+    strenghts = init_strenghts
     strenghts.each_cons(2).map { |a, b| (b - a).abs }.min
   end
 
-  def self.start
+  def init_strenghts
     n = gets.to_i
     strenghts_array = []
     n.times do
       pi = gets.to_i
       strenghts_array << pi
     end
-    strenghts_array
+    strenghts_array.sort
+  end
+
+  def start
+    puts find_difference
   end
 end
 
-p HorseRacing.find_difference
+obj = HorseRacing.new
+obj.start
