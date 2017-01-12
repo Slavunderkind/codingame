224785d32d1e0f214a1a4b69dbf699222c65cac4
Slavunderkind <smcankova@gmail.com>
2017-01-12 17:51:41 +0200

    Refactoring on init strengths method

--- Training/Easy/horse_racing.rb
+++ Training/Easy/horse_racing.rb
@@ -1,23 +1,18 @@
 # Solution for https://www.codingame.com/ide/puzzle/horse-racing-duals
 class HorseRacing
-  attr_accessor :horses_number
+  attr_accessor :horses_count
 
   def initialize
-    @horses_number = gets.to_i
+    @horses_count = gets.to_i
   end
 
   def find_difference
-    strenghts = init_strenghts
-    strenghts.each_cons(2).map { |a, b| (b - a).abs }.min
+    strengths = init_strengths
+    strengths.each_cons(2).map { |a, b| (b - a).abs }.min
   end
 
-  def init_strenghts
-    strenghts_array = []
-    horses_number.times do
-      horse_strenght = gets.to_i
-      strenghts_array << horse_strenght
-    end
-    strenghts_array.sort
+  def init_strengths
+    horses_count.times.each_with_object([]) { |_, result| result << gets.to_i }.sort
   end
 
   def start
