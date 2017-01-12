9229ce44a17f001b89a6654758bc614252ffd042
Slavunderkind <smcankova@gmail.com>
2017-01-12 11:22:42 +0200

    Add initialize method and change variable names

--- Training/Easy/horse_racing.rb
+++ Training/Easy/horse_racing.rb
@@ -1,16 +1,21 @@
 # Solution for https://www.codingame.com/ide/puzzle/horse-racing-duals
 class HorseRacing
+  attr_accessor :horses_number
+
+  def initialize
+    @horses_number = gets.to_i
+  end
+
   def find_difference
     strenghts = init_strenghts
     strenghts.each_cons(2).map { |a, b| (b - a).abs }.min
   end
 
   def init_strenghts
-    n = gets.to_i
     strenghts_array = []
-    n.times do
-      pi = gets.to_i
-      strenghts_array << pi
+    horses_number.times do
+      horse_strenght = gets.to_i
+      strenghts_array << horse_strenght
     end
     strenghts_array.sort
   end
