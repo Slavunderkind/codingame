756300834b9a054a01ffea6085a9634ccf04ec86
Slavunderkind <smcankova@gmail.com>
2017-01-23 16:24:00 +0200

    Refactor not_possible? method Put print action out of the class and use each_with_object to fill array with contributions

--- Training/Medium/gift.rb
+++ Training/Medium/gift.rb
@@ -9,34 +9,28 @@ class Gift
   end
 
   def init_budgets_list
-    list = []
-    participants.times do
-      list << gets.to_i
-    end
-    list.sort
+    Array.new(participants) { gets.to_i }.sort
   end
 
   def not_possible?
-    if budgets_list.inject(0, :+) < gift_price
-      puts 'IMPOSSIBLE'
-      true
-    else
-      false
-    end
+    budgets_list.inject(0, :+) < gift_price
   end
 
   def start
-    return if not_possible?
+    if not_possible?
+      puts 'IMPOSSIBLE'
+      return
+    end
     money_to_pay = gift_price
-    budgets_list.each_with_index do |budget, index|
+    budgets_list.each_with_index.each_with_object([]) do |(budget, index), result|
       contributions_count = budgets_list.size - index
       contribution = (money_to_pay / contributions_count).to_i
       contribution = budget if budget < contribution
       money_to_pay -= contribution
-      puts contribution
+      result << contribution
     end
   end
 end
 
 obj = Gift.new
-obj.start
+puts obj.start
