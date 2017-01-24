b091ce241f9c3700a18b860200cc9ee07699978d
Slavunderkind <smcankova@gmail.com>
2017-01-24 15:32:51 +0200

    Refactor the code

--- Training/Easy/mime_type.rb
+++ Training/Easy/mime_type.rb
@@ -1,16 +1,16 @@
 # Solution for https://www.codingame.com/ide/puzzle/mime-type
 class MimeType
-  attr_accessor :n, :q, :associations
+  attr_reader :elements_count, :files_count, :associations
 
-  def initialize
-    @n = gets.to_i # Number of elements which make up the association table.
-    @q = gets.to_i # Number Q of file names to be analyzed.
+  def initialize(elements_count, files_count)
+    @elements_count = elements_count
+    @files_count = files_count
     @associations = init_associations
   end
 
   def init_associations
     associations = {}
-    n.times do
+    elements_count.times do
       ext, mt = gets.split(' ') # ext: file extension, mt: MIME type.
       associations[ext.downcase] = mt
     end
@@ -19,21 +19,22 @@ class MimeType
 
   def match_file(filename)
     unknown = 'UNKNOWN'
-    splitted_filename = filename.split('.')
-    if splitted_filename.size > 1 && filename[-1] != '.'
-      result = associations.fetch(splitted_filename.last.downcase, nil)
+    name_and_extension = filename.split('.')
+    if name_and_extension.size > 1 && filename[-1] != '.'
+      result = associations[name_and_extension.last.downcase]
     end
-    result = unknown if result.nil?
-    result
+    result ||= unknown
   end
 
   def start
-    q.times do
+    files_count.times do
       filename = gets.chomp # One file name per line.
       puts match_file(filename)
     end
   end
 end
 
-obj = MimeType.new
+elements_count = gets.to_i
+files_count = gets.to_i
+obj = MimeType.new(elements_count, files_count)
 obj.start
