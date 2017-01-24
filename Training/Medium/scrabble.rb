638fceb8accb33e853e60e6f87bda6d7e7a5059c
Slavunderkind <smcankova@gmail.com>
2017-01-24 12:23:52 +0200

    Refactor the code

--- Training/Medium/scrabble.rb
+++ Training/Medium/scrabble.rb
@@ -1,24 +1,24 @@
 # Solution for https://www.codingame.com/ide/puzzle/scrabble
 class Scrabble
-  attr_accessor :words_count, :dictionary, :letters, :letters_weight
+  attr_accessor :dictionary, :letters, :letters_weight
 
   def initialize
-    @n = gets.to_i
-    @dictionary = @n.times.each_with_object([]) { |_, result| result << gets.chomp }
+    words_number = gets.to_i
+    @dictionary = Array.new(words_number) { gets.chomp }
     @letters = gets.chomp
     @letters_weight = init_letters_weight
   end
 
   def init_letters_weight
-    letters_weight = {}
-    letters_weight[1] = %w(e a i o n r t l s u)
-    letters_weight[2] = %w(d g)
-    letters_weight[3] = %w(b c m p)
-    letters_weight[4] = %w(f h v w y)
-    letters_weight[5] = ['k']
-    letters_weight[8] = %w(j x)
-    letters_weight[10] = %w(q z)
-    letters_weight
+    {
+      1 => %w(e a i o n r t l s u),
+      2 => %w(d g),
+      3 => %w(b c m p),
+      4 => %w(f h v w y),
+      5 => %w(k),
+      8 => %w(j x),
+      10 => %w(q z)
+    }
   end
 
   def matching?(word)
@@ -29,9 +29,8 @@ class Scrabble
   end
 
   def find_all_words
-    words_with_points = {}
-    dictionary.find_all do |word|
-      words_with_points[word] = calculate_points(word) if matching?(word)
+    words_with_points = dictionary.each_with_object({}) do |word, result|
+      result[word] = calculate_points(word) if matching?(word)
     end
     words_with_points.key(words_with_points.values.max)
   end
@@ -45,11 +44,8 @@ class Scrabble
     end
     points
   end
-
-  def start
-    puts find_all_words
-  end
 end
 
 obj = Scrabble.new
-obj.start
+answer = obj.find_all_words
+puts answer
