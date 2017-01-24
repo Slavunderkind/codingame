510c9167f64138adda159575bcde27f2d972506f
Slavunderkind <smcankova@gmail.com>
2017-01-24 14:57:49 +0200

    Refactor the code

--- Training/Easy/horse_racing.rb
+++ Training/Easy/horse_racing.rb
@@ -1,24 +1,21 @@
 # Solution for https://www.codingame.com/ide/puzzle/horse-racing-duals
 class HorseRacing
-  attr_accessor :horses_count
+  attr_reader :horses_count, :strengths
 
-  def initialize
-    @horses_count = gets.to_i
-  end
-
-  def find_difference
-    strengths = init_strengths
-    strengths.each_cons(2).map { |a, b| (b - a).abs }.min
+  def initialize(horses_count)
+    @horses_count = horses_count
+    @strengths = init_strengths
   end
 
   def init_strengths
-    horses_count.times.each_with_object([]) { |_, result| result << gets.to_i }.sort
+    Array.new(horses_count) { gets.to_i }.sort
   end
 
-  def start
-    puts find_difference
+  def find_difference
+    strengths.each_cons(2).map { |a, b| (b - a).abs }.min
   end
 end
 
-obj = HorseRacing.new
-obj.start
+horses_count = gets.to_i
+obj = HorseRacing.new(horses_count)
+puts obj.find_difference
