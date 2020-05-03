4922313c89a61fbff3d944210c27796f20192e85
slavunderkind <smcankova@gmail.com>
2020-05-03 14:42:01 +0300

    Remove not needed argument at #find_max_profit and delete extra blank lines

--- Training/StockTask/stock.rb
+++ Training/StockTask/stock.rb
@@ -9,18 +9,16 @@ class Stock
     @max_profits = []
   end
 
-
-  def find_max_profit(prices)
+  def find_max_profit
     prices.max - prices.take(max_price_index).min
   end
-
 
   def execute
     while prices.any?
       if max_price_index.zero?
         prices.delete(prices.max)
       else
-        max_profits << find_max_profit(prices)
+        max_profits << find_max_profit
         self.prices = prices.drop(max_price_index + 1)
       end
     end
@@ -31,9 +29,5 @@ class Stock
 
   def max_price_index
     prices.each_with_index.max.last
-  end
-
-  def delete_max_when_first(max)
-    prices.delete(prices.max)
   end
 end
