dbe93e76721b46848c5e4b26ae7500b2416f61e7
slavunderkind <smcankova@gmail.com>
2020-05-07 12:51:22 +0300

    Add Project model

--- Training/to_do_list/db/schema.rb
+++ Training/to_do_list/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_125331) do
+ActiveRecord::Schema.define(version: 2020_05_06_163019) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
