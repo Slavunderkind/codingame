334c6d2d3d56cff34600c3a74ece1a69c63bd2a3
slavunderkind <smcankova@gmail.com>
2020-05-07 13:02:14 +0300

    Add projects controller and index view

--- Training/to_do_list/config/routes.rb
+++ Training/to_do_list/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :projects
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
