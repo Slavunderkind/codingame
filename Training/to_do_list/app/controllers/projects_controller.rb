2e40d367ced06a882fa55fcc9aa86595d56f75c8
slavunderkind <smcankova@gmail.com>
2020-05-07 23:19:09 +0300

    Add missing templates at projects folder. Add #destroy at ProjectController. Render projects list at homepage

--- Training/to_do_list/app/controllers/projects_controller.rb
+++ Training/to_do_list/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -21,9 +23,24 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def project_params
     params.require(:project).permit(:name)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
